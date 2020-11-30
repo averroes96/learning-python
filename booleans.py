8c37bba542cb5f87364c5ca4e70eb7c03df56e2a
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2020-11-30 21:57:19 +0100

    Learned python's booleans

---
+++ booleans.py
@@ -0,0 +1,21 @@
+# True values
+
+print(bool("something"))
+print(bool(100))
+print(bool(100.2))
+print(bool([1,"2",3]))
+
+print("=" * 48)
+
+# False values
+
+print(bool(0))
+print(bool(""))
+print(bool([]))
+print(bool(()))
+print(bool(None))
+
+# Boolean operators
+## - and &&
+## - or ||
+## - not !
