82ca7f422889e595f9418e6f9c2b016ce21f129a
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2020-12-01 20:09:50 +0100

    Learned operators
    Learned about assignment and comparison operators

---
+++ operators.py
@@ -0,0 +1,41 @@
+# Assignment operators
+
+## +=
+
+x = 10
+y = 20
+
+x += y # x = x + y
+
+print(x)
+
+print("=" * 48)
+
+## -=
+
+x = 20
+y = 10 
+
+x -= y ##
+
+print(x)
+
+print("=" * 48)
+
+# Comparaison opeartors
+
+## == && !=
+
+print(100 == 100)
+print(100 == 200)
+print(100 == 100.0)
+print(100 != 200)
+print(100 != 100.0)
+
+print("=" * 48)
+
+## greater than >
+## less than <
+## greater or equal >=
+## less or equal <=
+
