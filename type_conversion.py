cb04c664a7301287ee62749cbc13097a39c74f55
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2020-12-01 21:02:04 +0100

    Type conversion
    Learned about type conversion concerning list,sets,tuples and dicts

---
+++ type_conversion.py
@@ -0,0 +1,32 @@
+# str(var) // convert to string
+
+print(type(str(float)))
+
+print("=" * 48)
+
+# tuple(iterable) // convert to tuple any iterable data type
+
+print(tuple({ "A", "B", "C"}))
+
+print("=" * 48)
+
+# list(iterable) // convert an iterable into a list
+
+print(list(( "A", "B", "C")))
+
+print("=" * 48)
+
+# set(iterable) // convert an iterable into a set
+
+print(set("Ada"))
+
+print("=" * 48)
+
+# dict() // convert a 2 dim list or tuple into a dict
+
+print(dict([["1",1], ["2",2], ["3", 3]]))
+print(dict((
+    (1,1),
+    (2,2),
+    (3,3)
+)))
